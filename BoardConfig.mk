458b0a19fab362013a174a487f585361e6072b3d
iamnotstanley <iamnotstanley@gmail.com>
2017-03-19 16:04:53 +0100

    Cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,6 @@ TARGET_BOOTLOADER_BOARD_NAME := mt6582
 TARGET_NO_BOOTLOADER := true
 
 TARGET_BOARD_PLATFORM := mt6582
-#TARGET_BOARD_PLATFORM_GPU :=
 
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -21,23 +20,9 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-#BOARD_SYSTEMIMAGE_PARTITION_SIZE :=
-#BOARD_USERDATAIMAGE_PARTITION_SIZE :=
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# TWRP specific flags. Check out this thread for more info on them : http://tinyw.in/twrpflags
-
-# This may be needed if you're using CM and cloned twrp source to bootable/recovery-twrp. Ignore if you're on Omni like we recommend
-#RECOVERY_VARIANT := twrp
-# Fixes slanty graphics if they're present
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
-# External storage stuff. Adapt it for your device
-#TW_EXTERNAL_STORAGE_PATH := 
-#TW_EXTERNAL_STORAGE_MOUNT_POINT := 
-#TW_DEFAULT_EXTERNAL_STORAGE := true
-# Path to sysfs that controls brightness
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-# Maximum brightness
 TW_MAX_BRIGHTNESS := 255
 TW_THEME := portrait_hdpi
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
