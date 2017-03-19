268616aebea051a28a2bc44677c618d67cac5c38
iamnotstanley <iamnotstanley@gmail.com>
2017-03-19 14:42:20 +0100

    Fix tzdata error

--- device.mk
+++ device.mk
@@ -1,4 +1,5 @@
 PRODUCT_COPY_FILES += \
-    device/huawei/h30u10/kernel:kernel
+    device/huawei/h30u10/kernel:kernel \
+	bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 $(call inherit-product, build/target/product/full_base_telephony.mk)
