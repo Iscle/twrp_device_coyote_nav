63b7ed198eec43970338cb75d333ddff678eab27
iamnotstanley <iamnotstanley@gmail.com>
2017-03-19 13:54:34 +0100

    Change model name again because this a2a529e1abdc7fdecc53203acacc16011d59dbd6 didn't worked

--- omni_h30u10.mk
+++ omni_h30u10.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, device/huawei/h30u10/device.mk)
 
 PRODUCT_NAME := omni_h30u10
-PRODUCT_DEVICE := h30u10
+PRODUCT_DEVICE := H30-U10
 PRODUCT_BRAND := huawei
-PRODUCT_MODEL := H30-U10
+PRODUCT_MODEL := h30u10
 PRODUCT_MANUFACTURER := huawei
