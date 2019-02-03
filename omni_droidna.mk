2790d62d1b7b1f6ff5e272128370d6ae1aa37bde
Iscle <albertiscle9@gmail.com>
2019-02-03 22:03:54 +0000

    Fix typo

--- omni_droidna.mk
+++ omni_droidna.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-$(call inherit-product, device/huawei/droidna/device.mk)
+$(call inherit-product, device/mediatek/droidna/device.mk)
 
 PRODUCT_NAME := omni_droidna
 PRODUCT_DEVICE := droidna
-PRODUCT_BRAND := huawei
+PRODUCT_BRAND := mediatek
 PRODUCT_MODEL := droidna
 PRODUCT_MANUFACTURER := mediatek
